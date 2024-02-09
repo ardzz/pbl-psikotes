46647eba0127c4d97402751248629277ecb2eb72
Suko Tyas Pernanda <suko.tyas.pernanda@gmail.com>
2024-02-09 10:07:13 +0700

    Init Repository

---
+++ README.md
@@ -0,0 +1,52 @@
+
+<p  align="center"><a  href="https://laravel.com"  target="_blank"><img  src="https://ik.polines.ac.id/wp-content/uploads/2023/11/logo-web.png"  width="360"  alt="Laravel Logo"></a></p>  
+
+## PBL Template D3 Teknik Informatika & S.Tr. Teknologi Rekayasa Komputer
+
+Repository ini digunakan sebagai template aplikasi dasar yang akan digunakan untuk pelaksanaan Project-Based Learning pada kedua prodi di atas di Jurusan Teknik Elektro, Politeknik Negeri Semarang.
+Dengan menggunakan satu template yang sama, diharapkan proyek-proyek yang dihasilkan akan lebih mudah untuk dikelola.
+
+PBL template ini membutuhkan <i>minimum requirements</i>:
+- PHP 8.2
+- Laravel 9
+- MySQL 8.0
+
+Cara menggunakan template ini adalah sebagai berikut:
+- Kloning template ini menggunakan perintah:
+``
+git clone https://gitlab.com/sukotyasp/pbl-laravel-template.git {project-directory}
+``
+- Masuk ke``{project-directory}``, hapus folder tersembunyi bernama `` .git``.
+- Install dependency menggunakan composer dengan perintah
+``composer install``
+- Salin file ``.env.example`` menjadi ``.env``
+- Buat database sesuai yang anda butuhkan, kemudian sesuaikan entry berikut pada file ``.env``:
+```
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=laravel
+DB_USERNAME=root
+DB_PASSWORD=root
+```
+- Jalankan perintah berikut:
+```
+php artisan migrate
+php artisan db:seed
+```
+- Jalankan aplikasi menggunakan perintah
+```
+php artisan serve
+```
+<hr>
+
+Terima Kasih kepada:
+- Kaprodi D3 Teknik Informatika
+- Kaprodi S.Tr. Teknologi Rekayasa Komputer
+- Ketua Jurusan Teknik Elektro, Politeknik Negeri Semarang
+- Task Force PBL D3 Teknik Informatika & S.Tr. Teknologi Rekayasa Komputer
+<hr>
+credit: https://github.com/mjumain/RBAC-LARAVEL-9
+<p align="center">
+<a  href="https://laravel.com"  target="_blank"><img  src="https://ik.polines.ac.id/wp-content/uploads/2024/02/laravel-logo.jpg"  width="400"  alt="Laravel Logo"></a>
+</p>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>UM JAMBI</title>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="{{ asset('') }}plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('') }}plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    @stack('css')
+    <link rel="stylesheet" href="{{ asset('') }}dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <!-- Preloader -->
+    {{-- <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src="{{ asset('') }}dist/img/logo-umjambi.png" alt="UM Jambi Logo"
+            height="80" width="80">
+    </div> --}}
+    <div class="wrapper">
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i
+                            class="fas fa-bars"></i></a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ml-auto ">
+                <li class="nav-item dropdown user-menu">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                        <img src="../../dist/img/user2-160x160.jpg" class="user-image img-circle elevation-2"
+                            alt="User Image">
+                        <span class="d-none d-md-inline">{{ Auth::user()->name }}</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+
+                        <li class="user-header bg-info">
+                            <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-3" alt="User Image">
+                            <p>
+                                {{ Auth::user()->name }}
+                                <small>Politeknik Negeri Semarang</small>
+                            </p>
+                        </li>
+                        <li class="user-footer">
+                            <a href="{{ route('profil.index') }}" class="btn btn-default btn-flat">Profil</a>
+                            <a href="#" class="btn btn-default btn-flat float-right" data-toggle="modal"
+                                data-target="#modal-logout"><i class="fas fa-sign-out-alt"></i> <span>Keluar</span></a>
+                        </li>
+
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                        <i class="fas fa-expand-arrows-alt"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="modal fade" id="modal-logout" data-backdrop="static" tabindex="-1" role="dialog"
+            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+                <div class="modal-content">
+
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <div class="modal-body text-center">
+                            <h5>Apakah anda ingin keluar ?</h5>
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                            <button type="button" class="btn btn-sm btn-default btn-flat"
+                                data-dismiss="modal">Tidak</button>
+                            <a class="btn btn-sm btn-info btn-flat float-right" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                            this.closest('form').submit();"><span>Ya,
+                                    Keluar</span></a>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+
+        <aside class="main-sidebar main-sidebar-custom sidebar-dark-info elevation-4">
+            <a href="{{ url('') }}" class="brand-link">
+                <img src="{{ asset('') }}dist/img/logo-polines.png" alt="Logo Polines"
+                    class="brand-image elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light "><strong>{{ env('APP_NAME', 'PBL IK-TI Polines') }}</strong></span>
+            </a>
+            <div class="sidebar">
+                <nav class="mt-2">
+                    @include('layouts.sidebar')
+                </nav>
+            </div>
+
+            {{-- <div class="sidebar-custom">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a class="btn btn-info btn-block" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                this.closest('form').submit();"><i
+                            class="fas fa-sign-out-alt"></i> <span>Keluar</span></a>
+                </form>
+            </div> --}}
+        </aside>
+
+        <div class="content-wrapper">
+            @yield('content')
+        </div>
+
+        <footer class="main-footer">
+            <div class="float-right d-none d-sm-block">
+                <b>Version</b> 1.0.0
+            </div>
+            <strong>&copy; {{ date('Y') }} <i>Task Force</i> PBL IK-TRK Polines</strong>
+        </footer>
+    </div>
+
+    <script src="{{ asset('') }}plugins/jquery/jquery.min.js"></script>
+    <script src="{{ asset('') }}plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('') }}plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    @stack('js')
+    <script src="{{ asset('') }}dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
