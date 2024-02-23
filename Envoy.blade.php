5a01dacbbfac1dbb2cc4112de53e7e943047c6ac
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-02-23 09:21:36 +0700

    Setup CI/CD

---
+++ Envoy.blade.php
@@ -0,0 +1,21 @@
+@servers(['kelompoktiga' => 'cybxcom@103.76.129.70'])
+
+@task('deploy', ['on' => 'kelompoktiga'])
+if [ ! -d /home/cybxcom/psikotes ]; then
+    cd /home/cybxcom/
+    if ssh-keygen -F gitlab.com; then echo ''; else ssh-keyscan -H gitlab.com >> ~/.ssh/known_hosts && chmod 644 ~/.ssh/known_hosts; fi
+    git clone git@gitlab.com:pbl-2024/pbl-psikotes.git psikotes
+    cd psikotes
+    cp .env.example .env
+    /usr/local/php83/bin/php83 /usr/local/bin/composer install
+    /usr/local/php83/bin/php83 artisan key:generate
+    /usr/local/php83/bin/php83 artisan storage:link
+    ln -s /home/cybxcom/psikotes/public /home/cybxcom/domains/kelompoktiga.my.id/public_html
+else
+    echo "Directory already exists"
+    cd /home/cybxcom/psikotes
+    git pull origin master
+    /usr/local/php83/bin/php83 /usr/local/bin/composer install
+    /usr/local/php83/bin/php83 artisan clear
+fi
+@endtask
