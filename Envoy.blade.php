f8ff7474a69de86fcc86764c7aa3ac1ea97e8422
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-02-28 12:54:22 +0700

    Make dev and prod deployer

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,6 @@
 @servers(['kelompoktiga' => 'cybxcom@103.76.129.70'])
 
-@task('deploy', ['on' => 'kelompoktiga'])
+@task('deploy-on-production', ['on' => 'kelompoktiga'])
 if [ ! -d /home/cybxcom/psikotes ]; then
     cd /home/cybxcom/
     if ssh-keygen -F gitlab.com; then echo ''; else ssh-keyscan -H gitlab.com >> ~/.ssh/known_hosts && chmod 644 ~/.ssh/known_hosts; fi
@@ -18,4 +18,24 @@ else
     /usr/local/php83/bin/php83 /usr/local/bin/composer install
     /usr/local/php83/bin/php83 artisan clear
 fi
+@endtask
+
+@task('deploy-on-staging', ['on' => 'kelompoktiga'])
+    if [ ! -d /home/cybxcom/psikotes-staging ]; then
+        cd /home/cybxcom/
+        if ssh-keygen -F gitlab.com; then echo ''; else ssh-keyscan -H gitlab.com >> ~/.ssh/known_hosts && chmod 644 ~/.ssh/known_hosts; fi
+        git clone --branch staging git@gitlab.com:pbl-2024/pbl-psikotes.git psikotes-staging
+        cd psikotes-staging
+        cp .env.example .env
+        /usr/local/php83/bin/php83 /usr/local/bin/composer install
+        /usr/local/php83/bin/php83 artisan key:generate
+        /usr/local/php83/bin/php83 artisan storage:link
+        ln -s /home/cybxcom/psikotes-staging/public /home/cybxcom/domains/staging.kelompoktiga.my.id/public_html
+    else
+        echo "Directory already exists"
+        cd /home/cybxcom/psikotes-staging
+        git pull origin staging
+        /usr/local/php83/bin/php83 /usr/local/bin/composer install
+        /usr/local/php83/bin/php83 artisan clear
+    fi
 @endtask
