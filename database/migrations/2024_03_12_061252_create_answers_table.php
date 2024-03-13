d52687dd117bcc38214708d47eca28d680fd1fb1
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-03-13 10:40:24 +0700

    Add models and migrations

---
+++ database/migrations/2024_03_12_061252_create_answers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("question_id")
+                ->constrained()
+                ->references("id")
+                ->on("questions");
+            $table->foreignId("user_id")
+                ->constrained()
+                ->references("id")
+                ->on("users");
+            $table->boolean("answer")->nullable(); // T, F, or NULL (can not say)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
