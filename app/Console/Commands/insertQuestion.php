707cd78d7099c10d0876046d4b05daa24c272078
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-03-20 11:27:21 +0700

    Add question's content

---
+++ app/Console/Commands/insertQuestion.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Question;
+use Illuminate\Console\Command;
+
+class insertQuestion extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:insert-question';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $question_file = file_get_contents("indonesian_questions.json");
+        $questions = json_decode($question_file, true);
+        foreach ($questions as $question) {
+            $question = new Question([
+                'content' => $question,
+            ]);
+            if($question->save()){
+                $this->info('Question inserted: ' . $question->content);
+            }else{
+                $this->error('Failed to insert question: ' . $question->content);
+            }
+        }
+    }
+}
