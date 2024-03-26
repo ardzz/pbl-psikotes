113f2efc962456009ad7d4e4e28159c90dcc3d8d
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-03-26 15:46:57 +0700

    Add $fillable and user relationship

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -8,10 +8,17 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Exam extends Model
 {
+    protected $fillable = ["user_id", "start_time", "end_time"];
+
     use HasFactory;
 
     function question(): BelongsTo
     {
         return $this->belongsTo(Question::class);
+    }
+
+    function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
