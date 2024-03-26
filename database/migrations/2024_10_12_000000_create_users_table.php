4c45b8aa60557dee5f8f26b67b32c98ab4850028
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-03-26 15:45:24 +0700

    add nullable at exam_id

--- database/migrations/2024_10_12_000000_create_users_table.php
+++ database/migrations/2024_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->foreignId("exam_id")
+                ->nullable()
                 ->constrained()
                 ->references("id")
                 ->on("exams");
