86e27afb69c4813f40b827ae53267ee9c1ab7d61
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-03-26 19:57:37 +0700

    Refactor: Redirect to /login when user is unauthenticated

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -13,12 +13,12 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    /*protected function redirectTo($request)
+    protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
             return route('login');
         }
-    }*/
+    }
 
     protected function unauthenticated($request, array|AuthenticationException $exception)
     {
