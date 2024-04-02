4dd0515768cee8d38a0ab0044c535ca367388fb0
Dimas Irsyad Maulana <officialdimasim@gmail.com>
2024-04-03 01:02:38 +0700

    Update UI quiz page

--- public/quiz-assets/assets/js/custom.js
+++ public/quiz-assets/assets/js/custom.js
@@ -1,143 +1,173 @@
+var timerElement = document.getElementById("timer");
+var totalTime = 60 * 60; // 1 jam
+var timeRemaining = totalTime;
+
+function updateTimer() {
+    var hours = Math.floor(timeRemaining / 3600);
+    var minutes = Math.floor((timeRemaining % 3600) / 60);
+    var seconds = timeRemaining % 60;
+
+    timerElement.textContent =
+        (hours < 10 ? "0" : "") +
+        hours +
+        ":" +
+        (minutes < 10 ? "0" : "") +
+        minutes +
+        ":" +
+        (seconds < 10 ? "0" : "") +
+        seconds;
+
+    if (timeRemaining <= 0) {
+        clearInterval(timerInterval);
+        timerElement.textContent = "Waktu Habis";
+        // Lakukan sesuatu saat waktu habis
+    } else {
+        timeRemaining--;
+    }
+}
+
+var timerInterval = setInterval(updateTimer, 1000);
+
 // next prev
 var divs = $(".show-section fieldset");
 var now = 0; // currently shown div
 divs.hide().first().show(); // hide all divs except first
 
 function next() {
-  divs.eq(now).hide();
-  now = now + 1 < divs.length ? now + 1 : 0;
-  divs.eq(now).show(); // show next
-  console.log(now);
-  $(".stepSingle").eq(now).addClass("active");
-  $(".bgColor").css("height", 25 * now + "%");
+    divs.eq(now).hide();
+    now = now + 1 < divs.length ? now + 1 : 0;
+    divs.eq(now).show(); // show next
+    console.log(now);
+    $(".stepSingle").eq(now).addClass("active");
+    $(".bgColor").css("height", 25 * now + "%");
 }
 $(".prev").on("click", function () {
-  divs.eq(now).hide();
-  now = now > 0 ? now - 1 : divs.length - 1;
-  divs.eq(now).show(); // show previous
-  console.log(now);
+    divs.eq(now).hide();
+    now = now > 0 ? now - 1 : divs.length - 1;
+    divs.eq(now).show(); // show previous
+    console.log(now);
 
-  $(".option").addClass("animate");
-  $(".option").removeClass("animateReverse");
-  $(".stepSingle")
-    .eq(now + 1)
-    .removeClass("active");
-  $(".bgColor").css("height", 25 * now + "%");
+    $(".option").addClass("animate");
+    $(".option").removeClass("animateReverse");
+    $(".stepSingle")
+        .eq(now + 1)
+        .removeClass("active");
+    $(".bgColor").css("height", 25 * now + "%");
 });
 
 // quiz validation
 var checkedradio = false;
 
 function radiovalidate(stepnumber) {
-  var checkradio = $("#step" + stepnumber + " input")
-    .map(function () {
-      if ($(this).is(":checked")) {
-        return true;
-      } else {
-        return false;
-      }
-    })
-    .get();
+    var checkradio = $("#step" + stepnumber + " input")
+        .map(function () {
+            if ($(this).is(":checked")) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        .get();
 
-  checkedradio = checkradio.some(Boolean);
+    checkedradio = checkradio.some(Boolean);
 }
 
 $(document).ready(function () {
-  $(".stepSingle").eq(0).addClass("active");
-  $(".bgColor").css("height", "25%");
-  // check step1
-  $("#step1btn").on("click", function () {
-    radiovalidate(1);
+    $(".stepSingle").eq(0).addClass("active");
+    $(".bgColor").css("height", "25%");
+    // check step1
+    $("#step1btn").on("click", function () {
+        radiovalidate(1);
 
-    if (checkedradio == false) {
-      (function (el) {
-        setTimeout(function () {
-          el.children().remove(".reveal");
-        }, 3000);
-      })(
-        $("#error").append(
-          '<div class="reveal alert alert-danger">Choose an option!</div>'
-        )
-      );
+        if (checkedradio == false) {
+            (function (el) {
+                setTimeout(function () {
+                    el.children().remove(".reveal");
+                }, 3000);
+            })(
+                $("#error").append(
+                    '<div class="reveal alert alert-danger">Choose an option!</div>'
+                )
+            );
 
-      radiovalidate(1);
-    } else {
-      $("#step1 .option").removeClass("animate");
-      $("#step1 .option").addClass("animateReverse");
-      setTimeout(function () {
-        next();
-      }, 900);
-      // countresult(1);
-    }
-  });
-  // check step2
-  $("#step2btn").on("click", function () {
-    radiovalidate(2);
+            radiovalidate(1);
+        } else {
+            $("#step1 .option").removeClass("animate");
+            $("#step1 .option").addClass("animateReverse");
+            setTimeout(function () {
+                next();
+            }, 900);
+            // countresult(1);
+        }
+    });
+    // check step2
+    $("#step2btn").on("click", function () {
+        radiovalidate(2);
 
-    if (checkedradio == false) {
-      (function (el) {
-        setTimeout(function () {
-          el.children().remove(".reveal");
-        }, 3000);
-      })(
-        $("#error").append(
-          '<div class="reveal alert alert-danger">Choose an option!</div>'
-        )
-      );
+        if (checkedradio == false) {
+            (function (el) {
+                setTimeout(function () {
+                    el.children().remove(".reveal");
+                }, 3000);
+            })(
+                $("#error").append(
+                    '<div class="reveal alert alert-danger">Choose an option!</div>'
+                )
+            );
 
-      radiovalidate(2);
-    } else {
-      $("#step2 .option").removeClass("animate");
-      $("#step2 .option").addClass("animateReverse");
-      setTimeout(function () {
-        next();
-      }, 900);
-      // countresult(1);
-    }
-  });
-  // check step3
-  $("#step3btn").on("click", function () {
-    radiovalidate(3);
+            radiovalidate(2);
+        } else {
+            $("#step2 .option").removeClass("animate");
+            $("#step2 .option").addClass("animateReverse");
+            setTimeout(function () {
+                next();
+            }, 900);
+            // countresult(1);
+        }
+    });
+    // check step3
+    $("#step3btn").on("click", function () {
+        radiovalidate(3);
 
-    if (checkedradio == false) {
-      (function (el) {
-        setTimeout(function () {
-          el.children().remove(".reveal");
-        }, 3000);
-      })(
-        $("#error").append(
-          '<div class="reveal alert alert-danger">Choose an option!</div>'
-        )
-      );
+        if (checkedradio == false) {
+            (function (el) {
+                setTimeout(function () {
+                    el.children().remove(".reveal");
+                }, 3000);
+            })(
+                $("#error").append(
+                    '<div class="reveal alert alert-danger">Choose an option!</div>'
+                )
+            );
 
-      radiovalidate(3);
-    } else {
-      $("#step3 .option").removeClass("animate");
-      $("#step3 .option").addClass("animateReverse");
-      setTimeout(function () {
-        next();
-      }, 900);
-      // countresult(1);
-    }
-  });
-  // check step4
-  $("#sub").on("click", function () {
-    radiovalidate(4);
+            radiovalidate(3);
+        } else {
+            $("#step3 .option").removeClass("animate");
+            $("#step3 .option").addClass("animateReverse");
+            setTimeout(function () {
+                next();
+            }, 900);
+            // countresult(1);
+        }
+    });
+    // check step4
+    $("#sub").on("click", function () {
+        radiovalidate(4);
 
-    if (checkedradio == false) {
-      (function (el) {
-        setTimeout(function () {
-          el.children().remove(".reveal");
-        }, 3000);
-      })(
-        $("#error").append(
-          '<div class="reveal alert alert-danger">Choose an option!</div>'
-        )
-      );
+        if (checkedradio == false) {
+            (function (el) {
+                setTimeout(function () {
+                    el.children().remove(".reveal");
+                }, 3000);
+            })(
+                $("#error").append(
+                    '<div class="reveal alert alert-danger">Choose an option!</div>'
+                )
+            );
 
-      radiovalidate(4);
-    } else {
-      showresult();
-    }
-  });
+            radiovalidate(4);
+        } else {
+            showresult();
+        }
+    });
 });
--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -8,17 +8,18 @@
                 <div class="tittle-ujian">
                     <h1 class="">PSIKOTEST</h1>
                     <h2>No 13</h2>
+                    <div class="timer" id="timer">00:00:00</div>
                 </div>
                 <div class="question">
                     <h1>apa yang membuatmu marah?</h1>
                     <div class="options">
                         <div class="option animate">
                             <input type="radio" name="op3" value="No" />
-                            <label>No</label>
+                            <label>Yes</label>
                         </div>
                         <div class="option animate delay-100">
                             <input type="radio" name="op3" value="Yes" />
-                            <label>Yes</label>
+                            <label>No</label>
                         </div>
                     </div>
                 </div>
@@ -33,44 +34,46 @@
                     <h2>Nomor soal</h2>
                 </div>
                 <div class="list-nosoal">
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
-                    <div class="no-satu">1</div>
+                    <div class="nomer nomer-hijau">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer nomer-merah">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
+                    <div class="nomer">1</div>
                 </div>
-
-                <h3>hijau : aman</h3>
-                <h3>hijau : aman</h3>
-                <h3>hijau : aman</h3>
                 <div class="btn-no-group">
                     <div class="btn-prev-nomor">
-                        <a href="http:">Sebelumnya</a>
+                        <a href="http:">&#9664; </a> <!-- Karakter panah kiri -->
                     </div>
                     <div class="btn-next-nomor">
-                        <a href="http:">Setelahnya</a>
+                        <a href="http:">&#9654;</a> <!-- Karakter panah kanan -->
                     </div>
                 </div>
+
+                <h5><span style="background-color: gray;">abu abu</span> : <span class="color-description">belum dikerjakan</span></h5>
+                <h5><span style="background-color: green;">hijau</span> : <span class="color-description">aman</span></h5>
+                <h5><span style="background-color: red;">merah</span> : <span class="color-description">ragu</span></h5>
+
+
             </div>
         </div>
 @endsection
