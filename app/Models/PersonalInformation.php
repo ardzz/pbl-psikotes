521fbc198f838b7cef4f7ecec21b6853474454a1
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-04-14 04:58:02 +0700

    feat(model): add `$fillable` property

--- app/Models/PersonalInformation.php
+++ app/Models/PersonalInformation.php
@@ -9,6 +9,22 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class PersonalInformation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nik',
+        'occupation',
+        'birthdate',
+        'phone_number',
+        'religion',
+        'marital_status',
+        'education',
+        'sex',
+        'province',
+        'city',
+        'district',
+        'sub_district',
+        'address',
+    ];
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
