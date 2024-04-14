1f6e6275fe07faf9dfc8a0404d6caede37371aaa
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-04-14 14:10:36 +0700

    style!: add sidebar template

--- resources/views/includes/patient-navbar.blade.php
+++ resources/views/includes/patient-navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="sidebar-nav scroll-sidebar" data-simplebar>
-    <ul id="sidebarnav">
+    <ul id="sidebar">
         <!-- ============================= -->
         <!-- Home -->
         <!-- ============================= -->
@@ -11,12 +11,37 @@
         <!-- Dashboard -->
         <!-- =================== -->
         <li class="sidebar-item">
-            <a class="sidebar-link" href="./index.html" aria-expanded="false">
+            <a class="sidebar-link" href="{{ route('guides') }}" aria-expanded="false">
                   <span>
-                    <i class="ti ti-aperture"></i>
+                    <i class="ti ti-book"></i>
                   </span>
-                <span class="hide-menu">Modern</span>
+                <span class="hide-menu">Guide</span>
             </a>
         </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="#" aria-expanded="false">
+                  <span>
+                    <i class="ti ti-notebook"></i>
+                  </span>
+                <span class="hide-menu">Exam</span>
+                <span class="hide-menu badge rounded-pill bg-success fs-2 py-1 px-2">Coming soon</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="#" aria-expanded="false">
+                <span>
+                    <i class="ti ti-report-medical"></i>
+                </span>
+                <span class="hide-menu">Medical Record</span>
+                <span class="hide-menu badge rounded-pill bg-success fs-2 py-1 px-2">Coming soon</span>
+            </a>
+        </li>
+        <li class="sidebar-item">
+            <a class="sidebar-link" href="{{ route('profile.edit') }}" aria-expanded="false">
+                <span>
+                    <i class="ti ti-user"></i>
+                </span>
+                <span class="hide-menu">Personal Information</span>
+            </a>
     </ul>
 </nav>
