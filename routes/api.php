f0173551b3c7737064e491037cd21b633260b61b
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-04-14 12:51:28 +0700

    feat(api)!: add api routes

---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\Auth\PasswordController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RestAPI\Exam;
+use App\Http\Controllers\RestAPI\Patient;
+use App\Http\Controllers\RestAPI\PersonalInformationController;
+use App\Http\Controllers\RestAPI\Question;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the 'api' middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('api')->group(function () {
+    Route::prefix('question')->group(function () {
+        Route::get('/{id}', [Question::class, 'show'])->name('show_question');
+        Route::post('/question', [Question::class, 'store'])->name('store_question');
+    });
+
+    Route::prefix('exam')->group(function () {
+        Route::get('/delete', [Exam::class, 'delete'])->name('delete_exam');
+        Route::get('/start', [Exam::class, 'start'])->name('start_exam');
+        Route::post('/add', [Exam::class, 'add'])->name('add_exam');
+    });
+
+    Route::prefix('patient')->group(function () {
+        Route::get('/search/{name}', [Patient::class, 'searchByName'])->name('search_patient');
+        Route::post('/personal-information', [PersonalInformationController::class, 'update'])->name('edit_personal_information');
+    });
+
+    Route::prefix('user')->group(function () {
+        Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::post('/password', [PasswordController::class, 'update'])->name('password.update');
+    });
+})->middleware(['auth:sanctum', 'verified', 'api']);
