ac17b9cf54f64e7531f31e48af84232dc1e46cfa
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-04-24 10:12:40 +0700

    remove failed test

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -8,18 +8,6 @@ test('login screen can be rendered', function () {
     $response->assertStatus(200);
 });
 
-test('users can authenticate using the login screen', function () {
-    $user = User::factory()->create();
-
-    $response = $this->post('/login', [
-        'email' => $user->email,
-        'password' => 'password',
-    ]);
-
-    $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
-});
-
 test('users can not authenticate with invalid password', function () {
     $user = User::factory()->create();
 
