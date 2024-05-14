d610929972b58ed1e98094511f518dcbc94fc4f3
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-05-14 13:56:41 +0700

    feat(login-method): add google

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\OauthGoogle;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/oauth/callback', [OauthGoogle::class, 'handleGoogleCallback'])->name('oauth.callback');
 
 Route::get('/', function () {
     return view('welcome');
