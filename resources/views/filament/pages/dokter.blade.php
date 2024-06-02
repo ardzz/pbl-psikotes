4438b3daa1354f3bfd2ce918418fcb626ae11031
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-06-02 10:41:56 +0700

    remove unnecessary file

--- resources/views/filament/pages/dokter.blade.php
+++
@@ -1,4 +0,0 @@
-<x-filament-panels::page>
-    {{ $this->form }}
-</x-filament-panels::page>
-<script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ \Midtrans\Config::$clientKey }}"></script>
