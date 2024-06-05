19d3dfd5c0c3f997997475fbb1bcf77dac0d5ea2
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-06-05 20:40:45 +0700

    refactor: remove payment creation button

--- app/Filament/Resources/PaymentResource/Pages/ListPayments.php
+++ app/Filament/Resources/PaymentResource/Pages/ListPayments.php
@@ -9,11 +9,4 @@ use Filament\Resources\Pages\ListRecords;
 class ListPayments extends ListRecords
 {
     protected static string $resource = PaymentResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
 }
