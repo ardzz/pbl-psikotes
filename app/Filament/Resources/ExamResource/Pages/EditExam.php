bbf5aff644629dbba415fe8d0560288d9e9bfbc8
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-06-05 14:41:56 +0700

    feat: notification when payment & exam is verified

--- app/Filament/Resources/ExamResource/Pages/EditExam.php
+++ app/Filament/Resources/ExamResource/Pages/EditExam.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\ExamResource\Pages;
 
 use App\Filament\Resources\ExamResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\HtmlString;
 
 class EditExam extends EditRecord
 {
@@ -26,6 +28,14 @@ class EditExam extends EditRecord
                 $payment->paid_at = now();
                 $payment->save();
             }
+            $this->record->user->notify(Notification::make()
+                ->title('Pembayaran telah diverifikasi')
+                ->body(function (){
+                    return new HtmlString('Psikotes dengan keterangan <strong>'.$this->record->purpose.'</strong> telah diverifikasi, anda dapat mengerjakan psikotes sekarang.');
+                })
+                ->success()
+                ->toDatabase()
+            );
         }
     }
 }
